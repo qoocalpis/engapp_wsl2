2b4ec033c938cc4d1fa678c9006f43cf33e080ac
calpi-s <qoocalpis@yahoo.co.jp>
2022-11-03 19:10:31 +0900

    second_commit

--- engapp/urls.py
+++ engapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ListBookView
+from .views import ListBookView,DetailBookView,CreateBookView
 
 urlpatterns = [
-    path("",ListBookView.as_view(),name="home"),
+    path("book/",ListBookView.as_view()),
+    path("book/<int:pk>/detail",DetailBookView.as_view()),
+    path('book/create/',CreateBookView.as_view()),
 ]
--- engapp/views.py
+++ engapp/views.py
@@ -1,9 +1,20 @@
 from string import Template
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView,CreateView
 from .models import Book
 
 # Create your views here.
 class ListBookView(ListView):
-    template_name = "engapp/home.html"
+    template_name = "engapp/book_list.html"
     model = Book
+
+
+class DetailBookView(DetailView):
+    template_name = "engapp/book_detail.html"
+    model = Book
+
+
+class CreateBookView(CreateView):
+    template_name = "engapp/book_create.html"
+    model = Book
+    fields = ('title','text','category')
